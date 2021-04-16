e0b2b01548f1c14645f2f097cc94e189c2bee058
mathieu-neyret <mathieu.n@biosample.fr>
2021-04-16 11:23:24 +0200

    Ajout des commentaires dans le code

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -5,8 +5,10 @@ const port = 3000
 
 app.use(cors())
 
+// Création d'un array qui va stocker les élèments de la requête JSON
 var data = []
 
+// Déclaration des évènements
 data.push({
   titre: "Premier évènement",
   description: "La p'tite description du premier évènement",
@@ -19,12 +21,20 @@ data.push({
   dateHeureDebut: "17 avril 2021",
   dateHeureFin: "19 avril 2021",
 });
+data.push({
+  titre: "Troisième évènement",
+  description: "La p'tite description du troisième évènement",
+  dateHeureDebut: "20 avril 2021",
+  dateHeureFin: "23 avril 2021",
+});
 
+// Une boucle qui va mettre le tableau data dans une requête JSON à chaque fois qu'on va
+// appeler la page "events"
 app.get('/events', (req, res) => {
   res.json(data);
 })
 
-
+// Affichage de l'url + port du back dans la console
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
--- ExpressJS/front/indexFront.js
+++ ExpressJS/front/indexFront.js
@@ -1,6 +1,11 @@
+// Code js front qui sert à prendre les informations de la réponse JSON + les afficher
+
 function traiterSuccesFetch(reponse) {
+    // Fonction qui va s'exécuter si la requête Fetch + JSON se sont déroulés avec succès
     console.log(reponse);
     for(var i=0;i<reponse.length;i++){
+        // Boucle qui va chercher tout les éléments du Array de la requête JSON et les afficher
+        // dans des div (titre, description, date du début, date de la fin)
         document.getElementById("blockEvenement").innerHTML += `
         <div>
             <h2 class="titleEvenement">${reponse[i].titre}</h2>
@@ -21,12 +26,14 @@ function traiterSuccesFetch(reponse) {
 }
   
 function traiterErreurFetch(erreur) {
+    // Fonction qui va s'éxecuter si il y a une erreur dans la requête Fetch et/ou JSON
     console.log("erreur");
 }
 
 document.getElementById("monBouton").addEventListener("click", function() {
+    // Quand on clique sur le bouton, on va appeler la page "events" du back grâce à un Fetch
     fetch("http://localhost:3000/events")
-        .then(res => res.json())
-        .then(traiterSuccesFetch, traiterErreurFetch);
+        .then(res => res.json()) // Appeler la requête JSON du back
+        .then(traiterSuccesFetch, traiterErreurFetch); // Appel des fonctions ci-dessus
 });
 
